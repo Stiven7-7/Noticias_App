c2084b41f39739cce6d0622d266993487d08dd84
Brayan Pinzon <58951748+Stiven7-7@users.noreply.github.com>
2023-10-22 14:09:33 -0500

    Validacion formulario registro y funcionalidad

--- app/src/main/java/com/example/noticias_app/Registro.java
+++ app/src/main/java/com/example/noticias_app/Registro.java
@@ -1,14 +1,93 @@
 package com.example.noticias_app;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
+
+    EditText NombreET, CorreoET, ContraseñaET, ConfirmarContraseñaET;
+    Button RegistrarUsuario;
+    TextView TengoUnaCuentaTXT;
+
+    String nombre = "", correo = "", password = "", confirmarPassword = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Registrar");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+
+        NombreET = findViewById(R.id.NombreET);
+        CorreoET = findViewById(R.id.CorreoET);
+        ContraseñaET = findViewById(R.id.ContraseñaET);
+        ConfirmarContraseñaET = findViewById(R.id.ConfirmarContraseñaET);
+        RegistrarUsuario = findViewById(R.id.RegistrarUsuario);
+        TengoUnaCuentaTXT = findViewById(R.id.TengoUnaCuentaTXT);
+
+        RegistrarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+                ValidarDatos();
+            }
+        });
+
+        TengoUnaCuentaTXT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+                startActivity(new Intent(Registro.this, Login.class));
+                finish();
+            }
+        });
+    }
+
+    private void  ValidarDatos() {
+        nombre = NombreET.getText().toString();
+        correo = CorreoET.getText().toString();
+        password = ContraseñaET.getText().toString();
+        confirmarPassword = ConfirmarContraseñaET.getText().toString();
+
+        if(TextUtils.isEmpty(nombre)){
+            Toast.makeText(this,"Ingrese nombre", Toast.LENGTH_SHORT).show();
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+            Toast.makeText(this,"Ingrese correo", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this,"Ingrese contraseña", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(confirmarPassword)) {
+            Toast.makeText(this,"Confirme contraseña", Toast.LENGTH_SHORT).show();
+        } else if (!password.equals(confirmarPassword)) {
+            Toast.makeText(this,"Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this,"Registro Exitoso", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(Registro.this, MenuPrincipal.class));
+            finish();
+            //CrearCuenta();
+        }
+    }
+
+    private void CrearCuenta() {
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
